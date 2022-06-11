960ef1df1af34fb384fdf5cdefc58c77fbd5f8b3
ajnart <ajnart@pm.me>
2022-06-11 09:21:03 +0200

    :memo: Change members info

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,17 +1,53 @@
 export const FORMSPREE_URL = 'https://formspree.io/f/xjvlzork';
 export const Members = [
   {
-    name: 'Thomnas Camlong',
-    linkedin: 'https://www.linkedin.com/in/thomas-camlong/',
-    avatar: 'https://github.com/ajnart.png',
-    email: 'thomas@ajnart.fr',
-    title: 'Full Stack Developer',
+    name: 'Alexandre Quenelle',
+    linkedin: 'https://www.linkedin.com/in/alexandre-quenelle/',
+    avatar: '/images/members/alexandre.jpg',
+    email: 'alexandre.quenelle@epitech.eu',
+    title: 'Fontend developer',
   },
   {
     name: 'Corentin Mas',
-    linkedin: 'https://www.linkedin.com/in/Corentin-mas/',
-    avatar: 'https://github.com/vanerac.png',
+    linkedin: 'https://www.linkedin.com/in/corentinmas/',
+    avatar: '/images/members/corentin.jpg',
     email: 'corentin.mas@epitech.eu',
-    title: 'Back end and API developer',
+    title: 'Backend and API developer',
+  },
+  {
+    name: 'Thomnas Camlong',
+    linkedin: 'https://www.linkedin.com/in/thomas-camlong/',
+    avatar: '/images/members/thomas.jpg',
+    email: 'thomas@ajnart.fr',
+    title: 'Full Stack Developer and Devops engineer',
+  },
+  {
+    name: 'Grégoire Bézier',
+    linkedin: 'https://www.linkedin.com/in/gr%C3%A9goire-b%C3%A9zier-58ba59198/',
+    avatar:
+      'https://media-exp2.licdn.com/dms/image/C4E03AQGVn3OzX9TOmw/profile-displayphoto-shrink_200_200/0/1651616001538?e=1660176000&v=beta&t=2_COOg3dVcsRsuFqPlA8Aq5cpvGmtForPhFDZSFtbkU',
+    email: 'gregoire.bezier@epitech.eu',
+    title: 'AI and ML developer',
+  },
+  {
+    name: 'Axel Fuentes',
+    linkedin: 'https://www.linkedin.com/in/axel-fuentes-3079561a5/',
+    avatar: '/images/members/axel.jpg',
+    email: 'axel.fuentes@epitech.eu',
+    title: 'Frontend developer',
+  },
+  {
+    name: 'David Aphing-Kouassi',
+    linkedin: 'https://www.linkedin.com/in/david-aphing-kouassi-a7baa11a5/',
+    avatar: '/images/members/david.jpg',
+    email: 'david.aphing-kouassi@epitech.eu',
+    title: 'Backend developer',
+  },
+  {
+    name: 'Carlos Frangeard',
+    linkedin: 'https://www.linkedin.com/in/carlos-frangeard/',
+    avatar: '/images/members/carlos.jpg',
+    email: 'carlos.frangeard@epitech.eu',
+    title: 'Backend developer',
   },
 ];
