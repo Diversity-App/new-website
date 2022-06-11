c9c3d7c5400eea08632bd0e8307e3479e5cfb96a
ajnart <ajnart@pm.me>
2022-06-11 10:05:07 +0200

    :sparkles: Add Stats module

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,18 +5,43 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import HomePageHero from '../components/HomePageHero';
 import MemberCarousel from '../components/MembersCarousel';
 import MailchimpSubscribeForm from '../components/MailchimpSubscribe';
+import StatsGroup from '../components/StatsComponent';
 
 export function HomePage() {
-  const { t } = useTranslation(['common']);
+  const { t } = useTranslation(['index']);
+  const stats: { title: string; stats: string; description: string }[] = [
+    {
+      title: t('stats.active.title'),
+      stats: '49,6M',
+      description: t('stats.active.description'),
+    },
+    {
+      title: t('stats.time.title'),
+      stats: '2h25',
+      description: t('stats.time.description'),
+    },
+    {
+      title: t('stats.perperson.title'),
+      stats: '8.4',
+      description: t('stats.perperson.description'),
+    },
+    {
+      title: t('stats.diversified.title'),
+      stats: '47%',
+      description: t('stats.diversified.description'),
+    },
+  ];
+
   return (
     <div>
       <Container>
         <HomePageHero />
+        <StatsGroup data={stats} />
+        <Divider my="xl" variant="dotted" size="sm" />
         <Title order={3} mb="xl">
           {t('aboutus')}
         </Title>
         <MemberCarousel />
-        <Divider my="xl" variant="dotted" size="sm" />
         <MailchimpSubscribeForm />
       </Container>
     </div>
